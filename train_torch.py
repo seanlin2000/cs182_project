f7df4f30892f3c7355373ab2f3d4726d892f7e70
seanlin2000 <seanlin2000@berkeley.edu>
2021-05-10 22:07:47 -0700

    Added adversarial training to train_torch

--- train_torch.py
+++ train_torch.py
@@ -78,7 +78,7 @@ def main():
     # model_optimizer = optim.SGD(model_test.parameters(), lr=0.0001, momentum=0.9)
     model_exp_lr_scheduler = lr_scheduler.StepLR(model_optimizer, step_size=7, gamma=0.1)
 
-    model_loss_history = model_solver.train(model_optimizer, model_criterion, model_exp_lr_scheduler, num_epochs=25)
+    model_loss_history = model_solver.train(model_optimizer, model_criterion, None, num_epochs=25, adv_train=True)
     
     return model_test
 
