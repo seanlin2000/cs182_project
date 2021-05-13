b504eea02576b12f5d06915eba2b1cb91ea34aeb
seanlin2000 <seanlin2000@berkeley.edu>
2021-05-12 22:28:53 -0700

    Torch.nn.functional

--- models/convengers.py
+++ models/convengers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torch.functional as F
+import torch.nn.functional as F
 from collections import OrderedDict
 
 
